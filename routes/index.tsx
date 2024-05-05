db19c315c0ed16709f21459629777160fc1d8bbc
Eloy Castelo <hi@eloy.co>
2024-05-04 23:39:48 -0400

    chore: add links to linkedin and mail

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <>
-      <div class="">
+      <div class="justify-text bigger-text">
         <p class="">
-          Hey there!
+          <b>Hey there!</b>
         </p>
         <p class="">
           I'm Eloy, welcome to my corner of the Internet. This is my place to
@@ -18,13 +18,19 @@ export default function Home() {
         <p class="">
           Professionally, I started my carrer as Software Engineer back on 2016,
           and, since then, I have played different roles among different
-          companies and projects. You can find all my experience on my LinkedIn
-          profile.
+          companies and projects. You can find all my experience on&nbsp;
+          <a
+            href="https://linkedin.com/in/eloy-castelo/"
+            rel="noopener noreferer"
+            target="_blank"
+          >
+            my LinkedIn profile
+          </a>.
         </p>
         <p class="">
           If you would like to connect to me personally and/or professionally,
-          do not hesitate to reach me out at{" "}
-          <a href="mailto:hi@eloy.co">hi@eloy.co</a>
+          do not hesitate to reach me out at&nbsp;
+          <a href="mailto:hi@eloy.co">hi@eloy.co</a>.
         </p>
         <p class="">
           Kind regards, <br /> E.
